dfec8d2a5d4716c06d6af583b8ee7fe3ee10e916
Felipe Amaral <felipeamaraldev15@gmail.com>
2024-12-18 22:36:40 -0300

    variaveis de ambiente

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
-    port: 3330,
+    port: env.PORT,
     host: '0.0.0.0'
 }).then(() => {
-    console.log('Server is running on port 3330 🚀');
+    console.log(`Server is running on port ${env.PORT} 🚀`);
 })
