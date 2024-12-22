ef6993212f58b3caa1b6bc9101ba510d424e4aba
Felipe Amaral <felipeamaraldev15@gmail.com>
2024-12-22 17:22:32 -0300

    configurando estilização

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,18 +1,28 @@
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import {useFonts, Roboto_700Bold, Roboto_400Regular} from '@expo-google-fonts/roboto'
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { config } from './config/gluestack-ui.config';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
 
   const [fontsLoad] = useFonts({ Roboto_700Bold, Roboto_400Regular })
   return (
-    <View >
-      <StatusBar 
-        barStyle="dark-content"
+    <GluestackUIProvider config={config} >
+      <StatusBar
+        barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoad ? <Text>Home</Text> : <View /> }
-    </View>
+      
+      {fontsLoad ? (
+        <Center flex={1} bg="$gray700">
+          <Text>Home</Text>
+        </Center>
+      ) : (
+        <Loading />
+      )}
+    </GluestackUIProvider>
   );
 }
 
