874eb728cdaf28d5596db948912b78b651a96da9
Felipe Amaral <3482714l@cefetes.br>
2025-08-05 11:31:50 -0300

    testes no serviço de registro de profissionais

--- backend/src/Services/RegisterProfessionalServices.spec.ts
+++ backend/src/Services/RegisterProfessionalServices.spec.ts
@@ -1,6 +1,8 @@
 import {beforeEach, it, describe, expect} from 'vitest'
 import { InMemoruProfessionalRepository } from "../Repositories/in-memory/in-memory.professional.repository";
 import { RegisterProfessionalServices } from "./RegisterProfessionaServices";
+import { compare } from 'bcryptjs';
+import { ProfessionalAlreadyExistsError } from '../Errors/Professional/ProfessionaAlreadyExists';
 
 
 let professionalRepository: InMemoruProfessionalRepository;
@@ -23,5 +25,47 @@ describe('Register Professional', () => {
 
         expect(professional.id).toEqual(expect.any(Number))
     })
+
+    it('should hash user password upon registration', async () => {
+        
+        const {professional} = await sut.execute({
+            name: 'John Doe',
+            email: 'jhondoe@email.com',
+            adress:'rua do canto',
+            password: '123456',
+            phone: '11999999999'
+        })
+    
+        const isHashed = await compare(
+            '123456',
+            professional.password
+        )
+
+        console.log(isHashed)
+    
+        expect(isHashed).toBe(true);
+    
+        })
+    
+        it('should not be able to register with same email twice', async () => {
+        
+        const email = 'jhondoe@email.com'
+        await sut.execute({
+            name: 'John Doe',
+            email: email,
+            adress:'rua do canto',
+            password: '123456',
+            phone: '11999999999'
+        })
+    
+        await expect(() => sut.execute({
+            name: 'John Doe',
+            email: email,
+            adress:'rua do canto',
+            password: '123456',
+            phone: '11999999999'
+        })).rejects.toBeInstanceOf(ProfessionalAlreadyExistsError)
+    
+        })
 })
 
